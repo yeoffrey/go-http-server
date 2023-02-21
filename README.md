bf03d0580e2b890862cc093b3be31474ffde28b4
Geoffrey Belcher <geoffreybelcher@icloud.com>
2023-02-21 15:04:51 -0400

    Added support for multiple servers

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# go-http-server
+# Go HTTP Server
+
+## Setup
+
+## Description
+This project is a basic HTTP server that I made by following [this](https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go) Digital Ocean tutorial and then making my own modifications.
--- main.go
+++ main.go
@@ -1,20 +1,57 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"log"
+	"io"
+	"net"
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
+const keyServerAddr = "serverAddr"
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: Requested /\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "Root!\n")
+}
+func getBlog(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: Requested /blog\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "Blog!\n")
 }
 
-func handleRequest() {
-	http.HandleFunc("/home", homePage)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+func serveHttp(ctx context.Context, s *http.Server, ctxCancel context.CancelFunc) {
+	err := s.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+
+		fmt.Printf("server on port %s closed\n", s.Addr)
+	} else if err != nil {
+		fmt.Printf("error listening for server one: %s\n", err)
+	}
+	ctxCancel()
 }
 
 func main() {
-	handleRequest()
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/blog", getBlog)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go serveHttp(ctx, serverOne, cancelCtx)
+
+	<-ctx.Done()
 }
